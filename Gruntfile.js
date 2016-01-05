c2687317667a23e8062f604e4da6c4a3feca1872
Gloryofthe80s <gloryoftheeighties@gmail.com>
2016-01-04 22:13:43 -0600

    effectively eliminated need for Grunt, Sass is now compiled through Jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,18 +7,6 @@ module.exports = function( grunt ) {
     
     grunt.initConfig({
         
-        sass: {                              
-            dist: {                            
-                options: {                       
-                  style: 'expanded'
-                },
-                files: {                         
-                  'css/main.css': '_sass/main.scss'
-                }
-            }
-        }, //end sass
-
-        
         csslint : {
             test : {
                 options : {
@@ -30,47 +18,6 @@ module.exports = function( grunt ) {
             }
         }, //end csslint
 
-        
-        concat : {
-            dist : {
-                src : [ 'css/*.css' ],
-                dest : 'css/the-big-one.css',
-            }
-        }, //end concat
-
-        
-        cssmin : {
-            dist : {
-                src : 'css/main.css',
-                dest : 'css/main.min.css'
-            }
-        }, //end cssmin
-
-        
-        watch : {
-            files : [ 
-                        'index.html',
-                        'about.html',
-                        'archive.html',
-                        'contact.html',
-                        '_sass/*.scss',
-                        '_layouts/*.html',
-                        '_includes/*.html',
-                        '_posts/*.md',
-                        '_config.yml', 
-                    ],
-            tasks : [ 
-                        'sass'
-                    ],
-            options : {
-                spawn : false,
-                interrupt : true,
-                atBegin : true,
-                livereload : true
-            }
-        },//end watch
-        
-        
         shell : {
             jekyllBuild : {
                 command : 'jekyll build'
@@ -89,9 +36,7 @@ module.exports = function( grunt ) {
 
     
     // register custom grunt tasks
-    grunt.registerTask( 'serve', ['sass', 'shell:jekyllServe' ] );
     grunt.registerTask( 'test', [ 'csslint' ] );
-    grunt.registerTask( 'build', [ 'sass', 'concat', 'cssmin', 'shell:jekyllBuild' ] );
     grunt.registerTask( 'deploy', [ 'shell:ghPages' ] );
 
 };
